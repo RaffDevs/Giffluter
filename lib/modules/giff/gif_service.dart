6fb3dbb80731eb8e0fc33470818e475653dd37ba
Rafael Veronez <rafael.dias@luizalabs.com>
2021-11-04 17:53:30 -0300

    chore: Alteração do limit de gifs

--- lib/modules/giff/gif_service.dart
+++ lib/modules/giff/gif_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class GiffService {
-  final Uri _uri = Uri.parse("https://api.giphy.com/v1/gifs/trending?api_key=VDsuCMrdT3jjGIAMatID7XJuwRpAcaWu&limit=25&rating=g");
+  final Uri _uri = Uri.parse("https://api.giphy.com/v1/gifs/trending?api_key=VDsuCMrdT3jjGIAMatID7XJuwRpAcaWu&limit=19&rating=g");
 
   Future<Map> getGifs({required String search, required int offset}) async {
     if (search.isEmpty) {
@@ -17,7 +17,7 @@ class GiffService {
 
   Future<Map> getGifsBySearch({ required int offset, required String search }) async {
     const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=VDsuCMrdT3jjGIAMatID7XJuwRpAcaWu&";
-    final paramsUrl = "q=$search&limit=25&offset=$offset&rating=g&lang=en";
+    final paramsUrl = "q=$search&limit=19&offset=$offset&rating=g&lang=en";
     final Uri uri = Uri.parse("$baseUrl$paramsUrl");
 
     Response response = await get(uri);
