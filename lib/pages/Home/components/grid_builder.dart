db1cf7b6aad50fb12ee48e2cd78e1d8cae8b9e42
Rafael Veronez <rafael.dias@luizalabs.com>
2021-11-04 17:52:51 -0300

    feat: Implementação do botão "Carregar mais"

--- lib/pages/Home/components/grid_builder.dart
+++ lib/pages/Home/components/grid_builder.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:giffluter/ui/color_theme.dart';
 
 class GridBuilder extends StatelessWidget {
-  const GridBuilder({ 
-    Key? key,
-    required this.getGifs
-  }) : super(key: key);
+  const GridBuilder({Key? key, required this.getGifs}) : super(key: key);
 
   final Future getGifs;
 
@@ -15,20 +12,43 @@ class GridBuilder extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10
+        crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10
       ),
-      itemCount: data.length,
-      itemBuilder:(context, index) {
-        return GestureDetector(
-          child: Image.network(
-            data[index]["images"]["fixed_height"]["url"],
-            height: 300,
-            fit: BoxFit.cover,
-          ),
-          onTap: () {},
-        );
+      itemCount: data.length + 1,
+      itemBuilder: (context, index) {
+        if (index == data.length) {
+          return Card(
+            color: ColorTheme.backgorund,
+            elevation: 10,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.add,
+                  size: 50,
+                  color: ColorTheme.foreground,
+                ),
+                Text(
+                  'Carregar mais...',
+                  style: TextStyle(
+                    color: ColorTheme.foreground
+                  )
+                )
+              ],
+            ),
+          );
+        }
+        else {
+          return GestureDetector(
+            child: Image.network(
+              data[index]["images"]["fixed_height"]["url"],
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            onTap: () {},
+          );
+        }
+        
       },
     );
   }
@@ -39,7 +59,7 @@ class GridBuilder extends StatelessWidget {
       child: FutureBuilder(
         future: getGifs,
         builder: (context, snapshot) {
-          switch(snapshot.connectionState) {
+          switch (snapshot.connectionState) {
             case ConnectionState.waiting:
             case ConnectionState.none:
               return Container(
@@ -47,17 +67,17 @@ class GridBuilder extends StatelessWidget {
                 height: 200,
                 alignment: Alignment.center,
                 child: const CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(ColorTheme.foreground),
+                  valueColor:
+                      AlwaysStoppedAnimation<Color>(ColorTheme.foreground),
                   strokeWidth: 5,
                 ),
               );
             default:
               if (snapshot.hasError) {
                 return const Text('Error');
-              }
-              else {
+              } else {
                 return _gridGifs(context, snapshot);
-              } 
+              }
           }
         },
       ),
